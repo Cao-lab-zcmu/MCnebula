31f9060d7961f98d16ae34f40eb854dab35363d3
wizard <wizard@pop-os.localdomain>
2022-04-03 11:39:31 +0800

    new

--- R/method_filter_candidates_upon_classyfire.R
+++ R/method_filter_candidates_upon_classyfire.R
@@ -24,7 +24,7 @@ method_filter_candidates_upon_classyfire <-
     cat("## Method part: batch_get_classification (classyfireR::get_classification)\n")
     batch_get_classification(inchikey2d, tmp_pub, tmp_classyfire, ...)
     ## ---------------------------------------------------------------------- 
-    structure_set <- calculate_class_score(structure_set, nebula_name, ...)
+    structure_set <- calculate_class_score(structure_set, nebula_name, tmp_classyfire, ...)
     return(structure_set)
   }
 ## ---------------------------------------------------------------------- 
@@ -32,25 +32,34 @@ calculate_class_score <-
   function(
            structure_set,
            nebula_name,
+           dir,
            ...
            ){
+    ## get parent class of the nebula
     class_hierarchy <- mutate_get_parent_class(nebula_name, class_cutoff = 1) %>% 
       unlist(use.names = F) %>% 
       c(nebula_name, .) %>% 
-      data.table::data.table(Classification = ., hierarchy = length(.):1)
+      data.table::data.table(Classification = ., hierarchy = length(.):1) %>% 
+      dplyr::filter(hierarchy == 4)
     ## ------------------------------------- 
-    class_set <- extract_rdata_list(paste0(tmp_classyfire, "/class.rdata"), inchikey2d) %>%
+    inchikey2d <- unique(structure_set$inchikey2D)
+    ## ------------------ 
+    ## load existed class data
+    class_set <- extract_rdata_list(paste0(dir, "/class.rdata"), inchikey2d) %>%
       data.table::rbindlist(idcol = T) %>% 
       dplyr::rename(inchikey2d = .id) %>% 
+      ## merge with class hierarchy information
       merge(class_hierarchy, ., by = "Classification") %>% 
       dplyr::mutate(class_score = hierarchy * 2) %>% 
       dplyr::group_by(inchikey2d)
     ## calculate score
     class_score <- dplyr::summarise_at(class_set, "class_score", sum)
+    ## merge with origin structure_sete
     structure_set <- merge(structure_set, class_score,
                            by.x = "inchikey2D", by.y = "inchikey2d", all.x = T) %>% 
       dplyr::mutate(class_score = ifelse(is.na(class_score), 0, class_score)) %>% 
-      dplyr::arrange(.id, desc(score), desc(class_score)) %>% 
+      ## re-rank according to class_score
+      dplyr::arrange(.id, desc(class_score), desc(score)) %>% 
       dplyr::distinct(.id, .keep_all = T) %>% 
       dplyr::relocate(.id) %>% 
       dplyr::as_tibble()
@@ -152,8 +161,10 @@ batch_get_classification <-
                      return()
            })
     ## ------------------------------------- 
+    ## get classyfire classification
     df <- data.table::rbindlist(list)
     df <- dplyr::mutate(df, inchikey2d = stringr::str_extract(InChIKey, "^[A-Z]{1,}"))
+    ## use the function which writed based on classyfireR::get_classification
     auto_classyfire(df, dir_classyfire, ...)
     ## ------------------------------------- 
     ## gather classes
@@ -167,12 +178,26 @@ auto_classyfire <-
            classyfire_cl = NULL,
            ...
            ){
-    ## classyfireR
+    ## create access log
+    log_file <- paste0(dir_classyfire, "/log")
+    if(file.exists(log_file)){
+      log_df <- data.table::fread(log_file)
+      df <- dplyr::filter(df, !InChIKey %in% log_df$log)
+      if(nrow(df) == 0)
+        return()
+    }
+    ## ------------------------------------- 
     list <- by_group_as_list(df, "inchikey2d")
     ## this part can be multi-threads
-    pbapply::pblapply(list, base_auto_classyfire,
-                      dir_classyfire = dir_classyfire,
-                      cl = classyfire_cl)
+    log <- pbapply::pblapply(list, base_auto_classyfire,
+                             dir_classyfire = dir_classyfire,
+                             cl = classyfire_cl) %>% 
+      unlist(use.names = F) %>% 
+      data.table::data.table(log = .)
+    ## ------------------------------------- 
+    if(exists("log_df"))
+      log <- dplyr::bind_rows(log_df, log)
+    write_tsv(log, file = log_file)
   }
 base_auto_classyfire <-
   function(
@@ -180,9 +205,10 @@ base_auto_classyfire <-
            dir_classyfire
            ){
     inchikey2d <- df[1,][["inchikey2d"]]
-    lapply(df[["InChIKey"]], base2_classyfire,
-           inchikey2d = inchikey2d,
-           dir_classyfire = dir_classyfire)
+    log <- lapply(df[["InChIKey"]], base2_classyfire,
+                  inchikey2d = inchikey2d,
+                  dir_classyfire = dir_classyfire)
+    return(unlist(log, use.names = F))
   }
 base2_classyfire <-
   function(
@@ -191,17 +217,17 @@ base2_classyfire <-
            dir_classyfire
            ){
     file = paste0(dir_classyfire, "/", inchikey2d)
-    ch <- try(read_tsv(file), silent = T)
-    if(class(ch)[1] == "try-error"){
+    if(file.exists(file) == F){
       ## if not exists
       ch <- classyfireR::get_classification(inchikey)
     }else{
       return()
     }
     if(is.null(ch)){
-      return()
+      return(inchikey)
     }else{
       ch <- classyfireR::classification(ch)
       write_tsv(ch, file)
+      return()
     }
   }
